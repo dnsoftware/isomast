24ec0bab0c26a224b1aa7cb0048caf4e7771231e
daemon <dailyru@gmail.com>
2014-12-08 14:18:30 +0300

    Удаление лишнего

--- Шаблон/site_flash/js/cufon-replace.js
+++
@@ -1,3 +0,0 @@
-Cufon.replace(' nav li a', { fontFamily: 'Hattori Hanzo' , hover:true, textShadow:'1px 1px #d64708' });
-Cufon.replace('h2, .link , .link1', { fontFamily: 'Hattori Hanzo' , hover:true , textShadow:'1px 1px #a6a6a6' });
-Cufon.replace('h2 em', { fontFamily: 'Hattori Hanzo' , textShadow:'1px 1px #fac396' });
